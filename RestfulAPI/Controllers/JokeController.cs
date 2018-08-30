e9bbb08e8f4e4b379ae19c845b37aad02a8bf93d
unknown <jess7642@gmail.com>
2018-08-30 14:38:12 -0400

    Jokes and Northwind API calls

---
+++ RestfulAPI/Controllers/JokeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RestfulAPI.Controllers
+{
+    public class JokeController : ApiController
+    {
+        [HttpGet]     //<-- this is an attribute   can be a post if its something you'd like to add to the database. 
+       public string dadJokes()
+        {
+            string[] dadJokes = { "Did you hear about the restaurant on the moon? Great food, no atmosphere.", "How many apples grow on a tree? All of them.", "What do you call a fat psychic? A four-chin teller.", "What's brown and sticky? A stick." };
+
+        //randomly select joke
+        Random r = new Random();
+        int selected = r.Next(0, dadJokes.Length);
+
+
+        //return the joke
+        return dadJokes[selected];
+
+           
+
+        }
+        
+    }
+}
---
+++ RestfulAPI/Controllers/NorthwindController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RestfulAPI.Models;
+
+namespace RestfulAPI.Controllers
+{
+    public class NorthwindController : ApiController
+    {
+
+
+        //action to get info about all customers
+        public List<Customer> GetAllCustomers()
+        {
+            //create the ORM
+            NorthwindEntities ORM = new NorthwindEntities();
+
+
+            // get all customers form ORM
+            List<Customer> customerList = ORM.Customers.ToList();
+
+
+            //Return the list of customers
+            return customerList;
+
+
+        }
+        [HttpGet]
+        public List<Customer> GetCustomerByCity(string city)
+        {
+            //create orm
+            NorthwindEntities ORM = new NorthwindEntities();
+
+            //http://localhost:port/api/Northwind/GetCustomerByCity?city=Detroit
+
+            //get customers by city
+
+            return ORM.Customers.Where(x => x.City.ToLower() == city.ToLower()).ToList();
+            // ORM.Customers.Where(x=>x.City.ToLower() == city.ToLower());
+
+            //return customers by city list
+
+
+
+            // return customerByCity;
+
+        }
+        public List<string> GetAllCities()
+        {
+            NorthwindEntities ORM = new NorthwindEntities();
+            return ORM.Customers.Where(x => x.City != null).Select(x => x.City).Distinct().ToList();
+
+        }
+        [HttpGet]
+        public List<Order> GetCustomerOrders(string customerID)
+        {
+            NorthwindEntities ORM = new NorthwindEntities();
+
+            Customer c = ORM.Customers.Find(customerID);
+            if (c != null)
+            {
+
+                return c.Orders.ToList();
+
+            }
+            return null;    // if the customer is not found. 
+        }
+
+
+        //list all customers by country
+        [HttpGet]
+        public List<Customer> CustomersByCountry(string country)
+        {//http://localhost:port/api/Northwind/CustomersByCountry?country=France
+            NorthwindEntities ORM = new NorthwindEntities();
+
+            return ORM.Customers.Where(x => x.Country.ToLower() == country.ToLower()).ToList();
+
+        }
+        // action to get the last order made by a specific customer
+        [HttpGet]
+        public Order GetLastOrderOfCustomer(string customerID)
+        {
+           NorthwindEntities ORM = new NorthwindEntities();
+
+            Customer c = ORM.Customers.Find(customerID.ToUpper());
+            if (c != null)
+            {
+                List<Order> orders = c.Orders.OrderBy(x => x.OrderDate).ToList();
+
+                return orders[orders.Count - 1];
+            }
+            return null;
+        }
+
+
+    }
+}
